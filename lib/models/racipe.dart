6772fe7f17af4d66b1880bc8aefccad6f0202806
UDAYRAJ SINGH <82473873+udayrajlive@users.noreply.github.com>
2024-05-16 11:15:36 +0530

    Update racipe.dart
    old model had some issues with the casting,

--- lib/models/racipe.dart
+++ lib/models/racipe.dart
@@ -1,6 +1,3 @@
-import 'analyzed_instruction.dart';
-import 'extended_ingredient.dart';
-
 class Recipe {
   bool? vegetarian;
   bool? vegan;
@@ -10,14 +7,14 @@ class Recipe {
   bool? cheap;
   bool? veryPopular;
   bool? sustainable;
+  bool? lowFodmap;
   int? weightWatcherSmartPoints;
   String? gaps;
-  bool? lowFodmap;
+  int? preparationMinutes;
+  int? cookingMinutes;
   int? aggregateLikes;
-  double? spoonacularScore;
-  double? healthScore;
+  int? healthScore;
   String? creditsText;
-  String? license;
   String? sourceName;
   double? pricePerServing;
   List<ExtendedIngredient>? extendedIngredients;
@@ -28,133 +25,158 @@ class Recipe {
   String? sourceUrl;
   String? image;
   String? imageType;
+  // Taste? taste;
   String? summary;
   List<dynamic>? cuisines;
-  List<dynamic>? dishTypes;
-  List<dynamic>? diets;
+  List<String>? dishTypes;
+  List<String>? diets;
   List<dynamic>? occasions;
+  // WinePairing? winePairing;
   String? instructions;
   List<AnalyzedInstruction>? analyzedInstructions;
-  dynamic originalId;
-  String? spoonacularSourceUrl;
+  dynamic ? originalId;
+  double? spoonacularScore;
 
-  Recipe({
-    this.vegetarian,
-    this.vegan,
-    this.glutenFree,
-    this.dairyFree,
-    this.veryHealthy,
-    this.cheap,
-    this.veryPopular,
-    this.sustainable,
-    this.weightWatcherSmartPoints,
-    this.gaps,
-    this.lowFodmap,
-    this.aggregateLikes,
-    this.spoonacularScore,
-    this.healthScore,
-    this.creditsText,
-    this.license,
-    this.sourceName,
-    this.pricePerServing,
-    this.extendedIngredients,
-    this.id,
-    this.title,
-    this.readyInMinutes,
-    this.servings,
-    this.sourceUrl,
-    this.image,
-    this.imageType,
-    this.summary,
-    this.cuisines,
-    this.dishTypes,
-    this.diets,
-    this.occasions,
-    this.instructions,
-    this.analyzedInstructions,
-    this.originalId,
-    this.spoonacularSourceUrl,
-  });
+  Recipe(
+      {this.vegetarian,
+        this.vegan,
+        this.glutenFree,
+        this.dairyFree,
+        this.veryHealthy,
+        this.cheap,
+        this.veryPopular,
+        this.sustainable,
+        this.lowFodmap,
+        this.weightWatcherSmartPoints,
+        this.gaps,
+        this.preparationMinutes,
+        this.cookingMinutes,
+        this.aggregateLikes,
+        this.healthScore,
+        this.creditsText,
+        this.sourceName,
+        this.pricePerServing,
+        this.extendedIngredients,
+        this.id,
+        this.title,
+        this.readyInMinutes,
+        this.servings,
+        this.sourceUrl,
+        this.image,
+        this.imageType,
+        // this.taste,
+        this.summary,
+        this.cuisines,
+        this.dishTypes,
+        this.diets,
+        this.occasions,
+        // this.winePairing,
+        this.instructions,
+        this.analyzedInstructions,
+        this.originalId,
+        this.spoonacularScore});
 
-  factory Recipe.fromJson(json) => Recipe(
-        vegetarian: json['vegetarian'] as bool?,
-        vegan: json['vegan'] as bool?,
-        glutenFree: json['glutenFree'] as bool?,
-        dairyFree: json['dairyFree'] as bool?,
-        veryHealthy: json['veryHealthy'] as bool?,
-        cheap: json['cheap'] as bool?,
-        veryPopular: json['veryPopular'] as bool?,
-        sustainable: json['sustainable'] as bool?,
-        weightWatcherSmartPoints: json['weightWatcherSmartPoints'] as int?,
-        gaps: json['gaps'] as String?,
-        lowFodmap: json['lowFodmap'] as bool?,
-        aggregateLikes: json['aggregateLikes'] as int?,
-        spoonacularScore: json['spoonacularScore'] as double?,
-        healthScore: json['healthScore'] as double?,
-        creditsText: json['creditsText'] as String?,
-        license: json['license'] as String?,
-        sourceName: json['sourceName'] as String?,
-        pricePerServing: (json['pricePerServing'] as num?)?.toDouble(),
-        extendedIngredients: (json['extendedIngredients'] as List<dynamic>?)
-            ?.map((e) => ExtendedIngredient.fromJson(e))
-            .toList(),
-        id: json['id'] as int?,
-        title: json['title'] as String?,
-        readyInMinutes: json['readyInMinutes'] as int?,
-        servings: json['servings'] as int?,
-        sourceUrl: json['sourceUrl'] as String?,
-        image: json['image'] as String?,
-        imageType: json['imageType'] as String?,
-        summary: json['summary'] as String?,
-        cuisines: json['cuisines'] as List<dynamic>?,
-        dishTypes: json['dishTypes'] as List<dynamic>?,
-        diets: json['diets'] as List<dynamic>?,
-        occasions: json['occasions'] as List<dynamic>?,
-        instructions: json['instructions'] as String?,
-        analyzedInstructions: (json['analyzedInstructions'] as List<dynamic>?)
-            ?.map((e) => AnalyzedInstruction.fromJson(e))
-            .toList(),
-        originalId: json['originalId'] as dynamic?,
-        spoonacularSourceUrl: json['spoonacularSourceUrl'] as String?,
-      );
+  Recipe.fromJson(Map<String, dynamic> json) {
+    vegetarian = json['vegetarian'];
+    vegan = json['vegan'];
+    glutenFree = json['glutenFree'];
+    dairyFree = json['dairyFree'];
+    veryHealthy = json['veryHealthy'];
+    cheap = json['cheap'];
+    veryPopular = json['veryPopular'];
+    sustainable = json['sustainable'];
+    lowFodmap = json['lowFodmap'];
+    weightWatcherSmartPoints = json['weightWatcherSmartPoints'];
+    gaps = json['gaps'];
+    preparationMinutes = json['preparationMinutes'];
+    cookingMinutes = json['cookingMinutes'];
+    aggregateLikes = json['aggregateLikes'];
+    healthScore = json['healthScore'];
+    creditsText = json['creditsText'];
+    sourceName = json['sourceName'];
+    pricePerServing = json['pricePerServing'];
+    if (json['extendedIngredients'] != null) {
+      extendedIngredients = <ExtendedIngredient>[];
+      json['extendedIngredients'].forEach((v) {
+        extendedIngredients!.add(ExtendedIngredient.fromJson(v));
+      });
+    }
+    id = json['id'];
+    title = json['title'];
+    readyInMinutes = json['readyInMinutes'];
+    servings = json['servings'];
+    sourceUrl = json['sourceUrl'];
+    image = json['image'];
+    imageType = json['imageType'];
+    // taste = json['taste'] != null ? new Taste.fromJson(json['taste']) : null;
+    summary = json['summary'];
+    cuisines: json['cuisines'] as List<dynamic>?;
+    dishTypes = json['dishTypes'].cast<String>();
+    diets = json['diets'].cast<String>();
+    occasions: json['occasions'] as List<dynamic>?;
+    // winePairing = json['winePairing'] != null
+    //     ? new WinePairing.fromJson(json['winePairing'])
+    //     : null;
+    instructions = json['instructions'];
+    if (json['analyzedInstructions'] != null) {
+      analyzedInstructions = <AnalyzedInstruction>[];
+      json['analyzedInstructions'].forEach((v) {
+        analyzedInstructions!.add(AnalyzedInstruction.fromJson(v));
+      });
+    }
+    originalId = json['originalId'];
+    spoonacularScore = json['spoonacularScore'];
+  }
 
-  toJson() => {
-        'vegetarian': vegetarian,
-        'vegan': vegan,
-        'glutenFree': glutenFree,
-        'dairyFree': dairyFree,
-        'veryHealthy': veryHealthy,
-        'cheap': cheap,
-        'veryPopular': veryPopular,
-        'sustainable': sustainable,
-        'weightWatcherSmartPoints': weightWatcherSmartPoints,
-        'gaps': gaps,
-        'lowFodmap': lowFodmap,
-        'aggregateLikes': aggregateLikes,
-        'spoonacularScore': spoonacularScore,
-        'healthScore': healthScore,
-        'creditsText': creditsText,
-        'license': license,
-        'sourceName': sourceName,
-        'pricePerServing': pricePerServing,
-        'extendedIngredients':
-            extendedIngredients?.map((e) => e.toJson()).toList(),
-        'id': id,
-        'title': title,
-        'readyInMinutes': readyInMinutes,
-        'servings': servings,
-        'sourceUrl': sourceUrl,
-        'image': image,
-        'imageType': imageType,
-        'summary': summary,
-        'cuisines': cuisines,
-        'dishTypes': dishTypes,
-        'diets': diets,
-        'occasions': occasions,
-        'instructions': instructions,
-        'analyzedInstructions':
-            analyzedInstructions?.map((e) => e.toJson()).toList(),
-        'originalId': originalId,
-        'spoonacularSourceUrl': spoonacularSourceUrl,
-      };
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['vegetarian'] = vegetarian;
+    data['vegan'] = vegan;
+    data['glutenFree'] = glutenFree;
+    data['dairyFree'] = dairyFree;
+    data['veryHealthy'] = veryHealthy;
+    data['cheap'] = cheap;
+    data['veryPopular'] = veryPopular;
+    data['sustainable'] = sustainable;
+    data['lowFodmap'] = lowFodmap;
+    data['weightWatcherSmartPoints'] = weightWatcherSmartPoints;
+    data['gaps'] = gaps;
+    data['preparationMinutes'] = preparationMinutes;
+    data['cookingMinutes'] = cookingMinutes;
+    data['aggregateLikes'] = aggregateLikes;
+    data['healthScore'] = healthScore;
+    data['creditsText'] = creditsText;
+    data['sourceName'] = sourceName;
+    data['pricePerServing'] = pricePerServing;
+    if (extendedIngredients != null) {
+      data['extendedIngredients'] =
+          extendedIngredients!.map((v) => v.toJson()).toList();
+    }
+    data['id'] = id;
+    data['title'] = title;
+    data['readyInMinutes'] = readyInMinutes;
+    data['servings'] = servings;
+    data['sourceUrl'] = sourceUrl;
+    data['image'] = image;
+    data['imageType'] = imageType;
+
+    data['summary'] = summary;
+    if (cuisines != null) {
+      data['cuisines'] = cuisines!.map((v) => v.toJson()).toList();
+    }
+    data['dishTypes'] = dishTypes;
+    data['diets'] = diets;
+    if (occasions != null) {
+      data['occasions'] = occasions!.map((v) => v.toJson()).toList();
+    }
+
+    data['instructions'] = instructions;
+    if (analyzedInstructions != null) {
+      data['analyzedInstructions'] =
+          analyzedInstructions!.map((v) => v.toJson()).toList();
+    }
+    data['originalId'] = originalId;
+    data['spoonacularScore'] = spoonacularScore;
+    return data;
+  }
 }
